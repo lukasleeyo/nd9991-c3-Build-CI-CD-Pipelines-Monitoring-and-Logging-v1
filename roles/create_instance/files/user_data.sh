2cd3d4563bb98dfa39537d2929236f39d020866f
leejiawei <jiawei3e1@gmail.com>
2020-03-29 00:08:04 +0800

    Update user_data.sh

--- roles/create_instance/files/user_data.sh
+++ roles/create_instance/files/user_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -x
 export DEBIAN_FRONTEND=noninteractive
-sudo apt update
+sudo apt update -y
 sudo locale-gen pt_BR.UTF-8
 sudo apt-get install -y nginx
 cat > /etc/nginx/sites-enabled/default << EOF
